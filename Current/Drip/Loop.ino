997d11e7e5a9bad03b3420e5040cd898060be29d
Jesse Scott <jesse@memelab.ca>
2013-04-27 15:02:00 -0700

    tweaks

--- Current/Drip/Loop.ino
+++ Current/Drip/Loop.ino
@@ -7,13 +7,13 @@ void loop() {
   // Test Sensor
   if(millis() % 500 == 0) {
     Serial.println("Let's Read Our Sensor...");
-    readSensor();
+    //readSensor();
   }
   
   // Loop Through The Strands
-  if(inRange) {
+  //if(inRange) {
     loopStrands();
-  }
+  //}
   
   // Light Them LEDs !!!
   FastSPI_LED.show();
